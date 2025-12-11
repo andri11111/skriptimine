3e22235a781e7ee704e04aaf111cef3b6dd430a7
Andri Tilsen <andri.tilsen@voco.ee>
2025-12-11 14:58:39 +0200

    3

--- praks3/yl3.sh
+++ praks3/yl3.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-
+echo -n "Mitu reisijat on grupis ja mitu kohta on bussis (Näiteks: 22 66): "
+#loeb variable
+read reisijad bussikohad
+#jagab reisijad bussikohtadega ära
+tais=$(($reisijad / $bussikohad))
+#arvutab jäägi
+yle=$(($reisijad % $bussikohad))
+#kontrollib kas ülejäänud on rohkem kui 0, kui on siis lisab ühe bussi juurde
+if test $yle -gt 0
+then
+        tais=$(($tais + 1))
+fi
+echo "Kokku on vaja $tais taisbussi"
 
