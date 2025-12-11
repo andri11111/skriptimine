1f1494af5f53135234f268e1a36ee5006a868790
Andri Tilsen <andri.tilsen@voco.ee>
2025-12-11 14:44:37 +0200

    ülesanne1

---
+++ praks2/yl1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#echo jätab tühjad read skripti sisse
+echo
+#väljastab tänase päeva, kuu ja aasta  sõnadena
+date +"Täna on %A, %B, %Y"
+echo
+#kellaaeg tundide ja minutitena
+date +"Kellaaeg on %H:%M"
+echo
+#väljastab kalendri
+ncal
